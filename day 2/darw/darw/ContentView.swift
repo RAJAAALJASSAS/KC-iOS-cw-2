8fdfb202f4bd91d21f6034001ec24a83c6a4bd43
رجاء الجساس <rajaaljassas2009@gmail.com>
2022-08-10 19:58:02 +0300

    cw2

---
+++ day 2/darw/darw/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  darw
+//
+//  Created by ابناء فهد on 30/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, my name is rajaa !")
+            .font(.title)
+            .fontWeight(.medium)
+            .foregroundColor(Color.purple)
+
+    
+            .padding(12.0)
+    
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ day 2/darw/darw/darwApp.swift
@@ -0,0 +1,17 @@
+//
+//  darwApp.swift
+//  darw
+//
+//  Created by ابناء فهد on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct darwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
